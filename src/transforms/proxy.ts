f3f31e7f10ed1a7a470c8de1ff5aed8ad6f19561
yangshuai <704807396@qq.com>
2020-12-11 10:26:09 +0800

    proxy示例

--- src/transforms/proxy.ts
+++ src/transforms/proxy.ts
@@ -1,14 +1,20 @@
 import { FileInfo, API, Options } from 'jscodeshift';
-import {ASTPath} from "jscodeshift/src/core";
+import { ASTPath } from "jscodeshift/src/core";
+
+// jscodeshift -t F:\pyramid-cli-new\src\transforms\proxy.ts F:\pyramid-cli-new\src\transforms\__testfixtures__\proxy.ts
 
 module.exports = (fileInfo: FileInfo, api: API, options: Options) => {
     const j = api.jscodeshift;
     const root = j(fileInfo.source);
 
     root
-        .find(j.CallExpression, {})
+        .find(j.Property, {
+            key: {
+                name: 'dev'
+            }
+        })
         .forEach((path: ASTPath) => {
-
+            console.log('11111111111');
         });
 
     return root.toSource();
